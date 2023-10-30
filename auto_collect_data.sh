478f4107b072107a3a3ed08b914630f0206742c0
wuhanstudio <wuhanstudio@qq.com>
2023-10-30 13:41:44 +0000

    [script] Add auto collect script

---
+++ auto_collect_data.sh
@@ -0,0 +1,25 @@
+INDEX=0
+for TOWN in 1 2 3 4 5 6 7
+do
+    for DATA in 0 1 2
+    do
+        echo "[${INDEX}] Collecting data: Town0 ${TOWN} Round ${DATA}"
+        python main.py --town ${TOWN} --index ${INDEX} --frame 300
+        INDEX=$((INDEX+1))
+    done
+done
+
+INDEX=0
+while IFS= read -r d;
+do
+    while IFS= read -r dd;
+    do
+        # echo "Generating ${INDEX}th Video: ${dd}"
+        printf -v INDEX_FILE "%04d" $INDEX
+        echo ffmpeg -y -framerate 10 -i "${dd}/%4d.png" "./data/gt/carla/carla_2d_box_train/${INDEX_FILE}.mp4" >> generate_mp4.sh
+        INDEX=$((INDEX+1))
+    done <<<$(find ${d}/image/* -prune -type d)
+done <<<$(find ./data/gt/carla/carla_2d_box_train/* -prune -type d)
+
+chmod u+x generate_mp4.sh
+./generate_mp4.sh
